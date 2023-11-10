7769d72088ec415c0038f6bfe942b6db8ad7549c
skewerz <140334398+skewerz@users.noreply.github.com>
2023-11-10 23:36:46 +0300

    Add files via upload

--- printf/sources/print_format.c
+++ printf/sources/print_format.c
@@ -6,7 +6,7 @@
 /*   By: kalturan <kalturan@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 21:41:49 by kalturan          #+#    #+#             */
-/*   Updated: 2023/11/10 21:43:54 by kalturan         ###   ########.fr       */
+/*   Updated: 2023/11/10 23:16:36 by kalturan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	print_format(char s, va_list ap)
 	else if (s == 's')
 		count += print_str(va_arg(ap, char *));
 	else if (s == 'p')
-		count += print_pointer(va_arg(ap, unsigned long int));
+		count += print_pointer(va_arg(ap, void *));
 	else if (s == 'd' || s == 'i')
 		count += print_digit(va_arg(ap, int));
 	else if (s == 'u')
--- printf/sources/print_pointer.c
+++ printf/sources/print_pointer.c
@@ -6,20 +6,40 @@
 /*   By: kalturan <kalturan@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 23:42:35 by kalturan          #+#    #+#             */
-/*   Updated: 2023/11/10 21:52:25 by kalturan         ###   ########.fr       */
+/*   Updated: 2023/11/10 23:17:45 by kalturan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
-int	print_pointer(unsigned long int i)
+static int	print_xx(unsigned long n)
 {
-	int				count;
+	int		count;
+	char	*base;
+
+	count = 0;
+	base = "0123456789abcdef";
+	if (n < 16)
+		return (count = write(1, &base[n], 1));
+	else
+	{
+		count += print_xx(n / 16);
+		count += print_xx(n % 16);
+		return (count);
+	}
+	return (count);
+}
+
+int	print_pointer(void *i)
+{
+	int					count;
+	unsigned long		a;
 
 	count = 2;
+	a = (unsigned long)i;
 	write(1, "0x", 2);
 	if (i == 0)
 		return (count += print_digit(0));
-	count += print_xl((unsigned int)i);
+	count += print_xx((unsigned long)a);
 	return (count);
 }
