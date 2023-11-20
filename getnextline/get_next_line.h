f92e4df7c118cefd7b17d9e159020643cd397df6
skewerz <140334398+skewerz@users.noreply.github.com>
2023-11-20 18:29:05 +0300

    Add files via upload

---
+++ getnextline/get_next_line.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kalturan <kalturan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/20 18:17:49 by kalturan          #+#    #+#             */
+/*   Updated: 2023/11/20 18:18:06 by kalturan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <fcntl.h>
+# include <stddef.h>
+# include <stdio.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *line, char *buff);
+char	*ft_strchr(char *s, int c);
+size_t	ft_strlen(char *s);
+char	*new_line(char *line);
+char	*ft_get_line(int fd, char *line);
+char	*ft_get_next_line(char	*line);
+
+#endif
---
+++ getnextline/get_next_line_bonus.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kalturan <kalturan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/20 18:23:28 by kalturan          #+#    #+#             */
+/*   Updated: 2023/11/20 18:23:34 by kalturan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# include <stdlib.h>
+# include <fcntl.h>
+# include <stddef.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <limits.h>
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *line, char *buff);
+char	*ft_strchr(char *s, int c);
+size_t	ft_strlen(char *s);
+char	*new_line(char *line);
+char	*ft_get_line(int fd, char *line);
+char	*ft_get_next_line(char	*line);
+
+#endif
